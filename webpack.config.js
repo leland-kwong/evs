c624776437e73cee146b981e9cf6d4e77de11b68
leland kwong <leland.kwong@gmail.com>
2020-02-27 00:13:15 -0500

    Remove unused files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const htmlGlobals = require('./src/internal/ldom-globals.json');
 
 module.exports = {
   mode: 'development',
