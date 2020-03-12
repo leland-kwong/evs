0b9cc9d8f6c8ca159224b555c7b71f5d11826ac0
leland kwong <leland.kwong@gmail.com>
2020-03-11 20:31:42 -0400

    Modularize

--- src/internal/auto-dom/hooks.js
+++ src/internal/auto-dom/hooks.js
@@ -46,89 +46,94 @@ const cleanupOnDestroy = (type, refId) => {
   }
 };
 
+const forceUpdate = (refId, dispatcher, currentProps) => {
+  const currentValue = getTreeValue(refId);
+  /**
+   * We know its a fragment if the returned value
+   * is a collection of vnodes
+   */
+  const isFragment = isArray(currentValue);
+  const nextValue = createElement(
+    [dispatcher, currentProps],
+    refId,
+  );
+
+  if (!isFragment) {
+    renderWith(currentValue, nextValue, refId, identity);
+    /**
+     * mutate original with tne new values since the
+     * source vtree will need the updates when we do
+     * a rerender of the full vtree.
+     */
+    Object.assign(currentValue, nextValue);
+    return;
+  }
+
+  /**
+   * Rerender the fragment by rerendering the
+   * parent vnode and updating its children
+   * using the new fragment.
+   */
+  const parentVnode = findParentVnode(refId);
+  const { children: oChildren } = parentVnode.props;
+  /**
+   * Update the old fragment with the new value
+   */
+  const newChildren = oChildren.map((ch) => {
+    const isCurrentFragment = isArray(ch)
+      && ch[0].refId === currentValue[0].refId;
+
+    if (isCurrentFragment) {
+      return nextValue;
+    }
+
+    return ch;
+  });
+  /**
+   * Create the new parent vnode by copying it and
+   * updating the original children props with the
+   * new children props.
+   */
+  const nextParentVnode = createVnode({
+    ...parentVnode,
+    props: {
+      ...parentVnode.props,
+      /**
+       * @important
+       * We must update the props in order to retain
+       * a proper historical copy of changes. Otherwise,
+       * if we modify the vnode's children directly, then
+       * the next render will not be accessing the latest
+       * children from the props. In other words, each
+       * update should have a new vnode where the props
+       * should be the representation of the change and
+       * `createVnode` will generate a new vnode based
+       * on the props.
+       */
+      children: newChildren,
+    },
+  });
+
+  const { $$refId: parentRefId } = parentVnode.props;
+  renderWith(parentVnode, nextParentVnode, parentRefId, identity);
+  Object.assign(parentVnode, nextParentVnode);
+};
+
 const useModel = (refId, initModel) => {
   const currentProps = getCurrentProps();
   const dispatcher = getCurrentDispatcher();
-  // console.log('[TodoMain | currentProps]', currentProps);
   const model = modelsByRefId.get(refId) || initModel();
-  const reRender = () => {
-    const currentValue = getTreeValue(refId);
-    /**
-     * We know its a fragment if the returned value
-     * is a collection of vnodes
-     */
-    const isFragment = isArray(currentValue);
-    const nextValue = createElement(
-      [dispatcher, currentProps],
-      refId,
-    );
-
-    if (!isFragment) {
-      renderWith(currentValue, nextValue, refId, identity);
-      /**
-       * mutate original with tne new values since the
-       * source vtree will need the updates when we do
-       * a rerender of the full vtree.
-       */
-      Object.assign(currentValue, nextValue);
-      return;
-    }
-
-    /**
-     * Rerender the fragment by rerendering the
-     * parent vnode and updating its children
-     * using the new fragment.
-     */
-    const parentVnode = findParentVnode(refId);
-    const { children: oChildren } = parentVnode.props;
-    /**
-     * Update the old fragment with the new value
-     */
-    const newChildren = oChildren.map((ch) => {
-      const isCurrentFragment = isArray(ch)
-        && ch[0].refId === currentValue[0].refId;
-
-      if (isCurrentFragment) {
-        return nextValue;
-      }
-
-      return ch;
-    });
-    /**
-     * Create the new parent vnode by copying it and
-     * updating the original children props with the
-     * new children props.
-     */
-    const nextParentVnode = createVnode({
-      ...parentVnode,
-      props: {
-        ...parentVnode.props,
-        /**
-         * @important
-         * We must update the props in order to retain
-         * a proper historical copy of changes. Otherwise,
-         * if we modify the vnode's children directly, then
-         * the next render will not be accessing the latest
-         * children from the props. In other words, each
-         * update should have a new vnode where the props
-         * should be the representation of the change and
-         * `createVnode` will generate a new vnode based
-         * on the props.
-         */
-        children: newChildren,
-      },
-    });
-
-    const { $$refId: parentRefId } = parentVnode.props;
-    renderWith(parentVnode, nextParentVnode, parentRefId, identity);
-    Object.assign(parentVnode, nextParentVnode);
-  };
 
   modelsByRefId.set(refId, model);
-  addWatch(model, 'reRender', reRender);
+  addWatch(model, 'reRender', () => {
+    forceUpdate(refId, dispatcher, currentProps);
+  });
   useHook(refId, cleanupOnDestroy);
 
   return model;
 };
 
-export { useModel };
+export {
+  useModel,
+  forceUpdate,
+};
